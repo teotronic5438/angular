b88d1461c38ead38dd07ad2b1972dfd6cd15b980
teotronic5438 <elias.orihuela>
2023-04-30 14:46:56 -0300

    video 19 @Input comunicacion de Padre a Hijo

--- app-empleados/src/app/app.module.ts
+++ app-empleados/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { EmpleadoHijoCComponent } from './empleado-hijo-c/empleado-hijo-c.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoHijoCComponent
   ],
   imports: [
     BrowserModule,
---
+++ app-empleados/src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent {
+  @Input() empleadosLista: Empleado[];   // defino la variable que viene del padre, que es tipo Empleado (class)
+
+}
